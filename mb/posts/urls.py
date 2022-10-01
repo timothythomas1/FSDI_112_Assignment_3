ee89547adeb95c1dd566c513d4ec290f7699f869
timothythomas1 <tuxes-parlay0s@icloud.com>
2022-09-30 19:28:16 -0700

    Updated home and about links to be within pages directory

--- mb/posts/urls.py
+++ mb/posts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, HomePageView
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'), # "name" tags are used for the anchor tags
     path('list', PostListView.as_view(), name='list'),
     path('<int:pk>', PostDetailView.as_view(), name='detail'),
     path('new/', PostCreateView.as_view(), name='new'),
--- mb/posts/views.py
+++ mb/posts/views.py
@@ -1,13 +1,10 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Post
 
 # class PostHome(HomePageView):
 #     template_name = "posts/home.html"
 #     model = Post
-class HomePageView(TemplateView): # Instantiates a new TemplateView class
-    template_name = "posts/home.html"
-
 class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
